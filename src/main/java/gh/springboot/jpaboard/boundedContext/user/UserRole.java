c1d6e4522470beeb0d22d28272cf09421a3f932f
geongeongeon <leegeonhee0204@naver.com>
2025-01-18 23:59:48 +0900

    SiteUser 앤티티 추가

---
+++ src/main/java/gh/springboot/jpaboard/boundedContext/user/UserRole.java
@@ -0,0 +1,14 @@
+package gh.springboot.jpaboard.boundedContext.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum UserRole {
+
+    ADMIN("ROLE_ADMIN"), USER("ROLE_USER");
+
+    private String role;
+
+}

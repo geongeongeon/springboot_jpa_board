0c0626a85b42c4ddf51bbd4423400138355b65f4
geongeongeon <leegeonhee0204@naver.com>
2025-01-20 20:51:11 +0900

    아이디, 이메일 중복 시 예외 발생 테스트 케이스 추가

--- src/test/java/gh/springboot/jpaboard/JpaBoardApplicationTests.java
+++ src/test/java/gh/springboot/jpaboard/JpaBoardApplicationTests.java
@@ -5,13 +5,16 @@ import gh.springboot.jpaboard.boundedContext.user.SiteUser;
 import gh.springboot.jpaboard.boundedContext.user.UserRepository;
 import gh.springboot.jpaboard.boundedContext.user.UserRole;
 import gh.springboot.jpaboard.boundedContext.user.UserService;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataIntegrityViolationException;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
 class JpaBoardApplicationTests {
@@ -45,6 +48,26 @@ class JpaBoardApplicationTests {
 		assertThat(findUser.getUsername()).isEqualTo(createUser.getUsername());
 		assertThat(findUser.getEmail()).isEqualTo(createUser.getEmail());
 		assertThat(findUser.getRole()).isEqualTo(createUser.getRole());
+	}
+
+	@Test
+	@DisplayName("아이디 중복 예외 발생")
+	void t002() {
+		SiteUser createUser = userService.createUser("user1", "1234", "user1@test.com", UserRole.USER);
+
+		assertThrows(DataIntegrityViolationException.class, () -> {
+			SiteUser duplicateUser = userService.createUser("user1", "1234", "user2@test.com", UserRole.USER);
+		});
+	}
+
+	@Test
+	@DisplayName("이메일 중복 예외 발생")
+	void t003() {
+		SiteUser createUser = userService.createUser("user1", "1234", "user1@test.com", UserRole.USER);
+
+		assertThrows(DataIntegrityViolationException.class, () -> {
+			SiteUser duplicateUser = userService.createUser("user2", "1234", "user1@test.com", UserRole.USER);
+		});
 	}
 
 }

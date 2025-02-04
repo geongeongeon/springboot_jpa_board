85a4e39522368f710ccb6044ef70f528a8e98801
geongeongeon <leegeonhee0204@naver.com>
2025-02-05 00:14:32 +0900

    디테일 화면 디자인 변경

---
+++ src/main/resources/static/js/detail.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+    $('#icon_comment').hover(function() {
+        $(this).removeClass('bi-chat');
+        $(this).addClass('bi-chat-fill');
+    }, function() {
+        $(this).removeClass('bi-chat-fill');
+        $(this).addClass('bi-chat');
+    });
+
+    $('#icon_like').hover(function() {
+        $(this).removeClass('bi-hand-thumbs-up');
+        $(this).addClass('bi-hand-thumbs-up-fill');
+    }, function() {
+        $(this).removeClass('bi-hand-thumbs-up-fill');
+        $(this).addClass('bi-hand-thumbs-up');
+    });
+
+    $('#icon_dislike').hover(function() {
+        $(this).removeClass('bi-hand-thumbs-down');
+        $(this).addClass('bi-hand-thumbs-down-fill');
+    }, function() {
+        $(this).removeClass('bi-hand-thumbs-down-fill');
+        $(this).addClass('bi-hand-thumbs-down');
+    });
+});
